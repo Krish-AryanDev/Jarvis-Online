38599da70945b514e0022205030ffa82b66d082e
Krish-AryanDev <krisharyan.dev@gmail.com>
2026-07-06 16:24:21 +0530

    used threading to run both camera and voice recognition simultaneously

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
 from camera import start_camera
+from voice import start_voice
+
+import threading
+
+voice_thread = threading.Thread(target = start_voice)
+
+voice_thread.start()
 
 start_camera()
+
+voice_thread.join()
